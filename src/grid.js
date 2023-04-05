4896d86e4e2bf96a19653897c29236ae3958171c
Maksym Nevdashchenko <nevmaks@gmail.com>
2023-04-05 20:51:49 +0300

    Structured application

---
+++ src/grid.js
@@ -0,0 +1,83 @@
+import PropTypes from "prop-types";
+import React, {cloneElement, memo, useCallback, useEffect, useRef, useState} from "react";
+
+const dataSource = [
+    {firstName: "John", lastName: "Doe", active: false},
+    {firstName: "Mary", lastName: "Moe", active: false},
+    {firstName: "Peter", lastName: "Noname", active: true}
+];
+
+function GridRecord ({record, toggleActive, index}) {
+    return (
+        <tr>
+            <td>{record.firstName}</td>
+            <td>{record.lastName}</td>
+            <td><input type="checkbox" checked={record.active} onChange={() => toggleActive(index)}/></td>
+        </tr>
+    )
+}
+
+const MemoGridRecord = memo(GridRecord);
+
+GridRecord.propTypes = {
+    record: PropTypes.shape({
+        firstName: PropTypes.string.isRequired,
+        lastName: PropTypes.string.isRequired,
+        active: PropTypes.bool.isRequired
+    })
+}
+
+GridRecord.defaultProps = {
+    record: {firstName: "N/A", lastName: "N/A", active: false}
+}
+
+export default function GridComponent({children}) {
+    let filterInput = useRef(null);
+
+    useEffect(() => {
+        filterInput.current.focus();
+    }, []);
+
+    const [records, setRecords] = useState(dataSource);
+
+    function toggleActive(index) {
+        dataSource[index] = { ...dataSource[index], active: !dataSource[index].active };
+        setRecords([...dataSource]);
+    }
+
+    const toggle = useCallback(toggleActive, []);
+
+    function handleFilterChange(e) {
+        let value = e.target.value;
+        setRecords(
+            dataSource.filter((record) =>
+                record.firstName.toUpperCase().includes(value.toUpperCase())
+            )
+        );
+    }
+
+    let recordsGrid = records.map((record, index) => {
+        return <MemoGridRecord record={record} key={index} index={index} toggleActive={ toggle } />
+    });
+
+    return (
+        <div style={{width: 300, height: 300, padding:20}}>
+            <p>
+                <input type="text" ref={filterInput} placeholder="Filter by..." onChange={handleFilterChange}/>
+            </p>
+            <table className="table table-condensed">
+                <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Active</th>
+                </tr>
+                </thead>
+                <tbody>
+                {recordsGrid}
+                </tbody>
+            </table>
+            <div>{ children && cloneElement( children, { records: records })}</div>
+        </div>
+    );
+}
